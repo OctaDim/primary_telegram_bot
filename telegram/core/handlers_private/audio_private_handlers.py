02690ba1f4c646c124f4a1a487b4ca45f016a6bd
Dima Mezhevich <octadim@gmail.com>
2024-04-29 01:48:01 +0300

    + audio private handler

--- telegram/core/handlers_private/audio_private_handlers.py
+++ telegram/core/handlers_private/audio_private_handlers.py
@@ -1,11 +1,13 @@
-from aiogram import Router, Bot, F
+from aiogram import Router, F
 from aiogram.types import Message
 
-from telegram.core.config.settings import BOT_CREDENTIALS
+from telegram.core.filters.chat_types_filter import ChatTypesFilter
 
 
 
 router_audio_private = Router()
+router_audio_private.message.filter(ChatTypesFilter(["private"]))
+
 
 @router_audio_private.message(F.audio)
 async def text_messages_handler(message: Message):
